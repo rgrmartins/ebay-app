21ecdac0e8da2ff6eaba7d54ce6f85c7de61ce43
roger <rogermartinsdesign@gmail.com>
2019-12-02 18:14:03 -0400

    controller creation

--- backend/src/app/controllers/AlertController.js
+++ backend/src/app/controllers/AlertController.js
@@ -1,5 +1,39 @@
-// import Alert from '../schemas/Alert';
+import * as Yup from 'yup';
+import Alert from '../schemas/Alert';
 
-class AlertController {}
+class AlertController {
+  async store(req, res) {
+    // input validation
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      search_phrase: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      research_time: Yup.number().required(),
+    });
+
+    // input validation
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fails.' });
+    }
+
+    // checking if search already exists for this email
+    const checkSearchExists = await Alert.findOne({
+      where: { email: req.body.email, searc_phrase: req.body.search_phrase },
+    });
+
+    if (checkSearchExists) {
+      return res
+        .status(401)
+        .json({ error: 'This survey has already been created by this email.' });
+    }
+
+    // save search and trigger API and email
+    // const search = await Alert.
+
+    return res.json();
+  }
+}
 
 export default new AlertController();
